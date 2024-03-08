96e463054bd987609aa80d4400a83b05412f0bc5
Daniel Ogbansiegbe <danogbans@gmail.com>
2024-03-08 23:09:35 +0100

    Update settings.py

--- converter/settings.py
+++ converter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7dubjlc8$1+s5von$n$-3k$6ptc*ck3k$v9t1$f!su7v(kebx4' 
+SECRET_KEY = 'django-insecure-7dubjlc8$1+s5von$n$-3k$6ptc*ck3k$v9t1$f!su7v(kebx4'  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
